e317e7a682f539415d9dc6884a43e40301d727fe
Junki Hori <junki326+github@gmail.com>
2023-05-27 19:02:15 +0900

    feat: Markdownファイルを元にインデックスページにブログポストを表示

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Head from "next/head";
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/post';
 
-export default function Home() {
+// getStaticProps()はサーバーサイドのビルド時に呼び出される
+// クライアントサイドでは呼び出されない
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
+
+export default function Home({ allPostsData }) {
+  // ビルド時に生成された````````````````'allPostsData'を使う （props.allDataPosts）
   return (
     <Layout home>
       <Head>
@@ -16,6 +29,20 @@ export default function Home() {
           (This is a sample website = you&apos;ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">out Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd}  ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
